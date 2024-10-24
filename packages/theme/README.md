00de1bcdd852e058662c4b1b541a975605ccccd0
Borghild <borghild.vikoyr@miles.no>
2024-10-24 08:30:42 +0200

    :art: packaging

---
+++ packages/theme/README.md
@@ -0,0 +1,25 @@
+# @equinor/energyvision-tailwind-preset
+
+Tailwind preset for Equinor Energyvision
+
+## Installation
+
+```bash
+  yarn add @equinor/energyvision-tailwind-preset
+  npm install @equinor/energyvision-tailwind-preset
+  pnpm add @equinor/energyvision-tailwind-preset
+```
+
+## Usage
+
+```javascript
+// tailwind.config.js
+
+module.exports = {
+  presets: [require('@equinor/energyvision-tailwind-preset')],
+}
+```
+
+## License
+
+[MIT](https://github.com/equinor/energyvision/blob/main/LICENSE)
---
+++ packages/theme/colorConfig.js
@@ -0,0 +1,34 @@
+/* 
+Web and Sanityv3 could import this file ?
+Sanityv3/colorConfig must be in sync with this
+*/
+module.exports = {
+  white: {
+    100: 'hsl(0, 0%, 100%)',
+  },
+  'moss-green': {
+    //moss-green-light
+    50: 'hsl(184, 30%, 96%)',
+  },
+  'spruce-wood': {
+    90: 'hsl(25, 100%, 94%)',
+  },
+  'mist-blue': {
+    100: 'hsl(199, 58%, 90%)',
+  },
+  'energy-red': {
+    100: 'hsl(348, 100%, 54%)',
+  },
+  green: {
+    50: '#c3e4ce',
+  },
+  yellow: {
+    50: '#fff5b8',
+  },
+  blue: {
+    50: '#49709c',
+  },
+  orange: {
+    50: '#f8d1af',
+  },
+}
